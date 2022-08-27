5788dc51b083bb2baebd3ffda8d6f865e27917a2
sabyr-cpu <sab.sabyr@gmail.com>
2022-08-27 23:09:14 +0600

    set utf-8

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ files = next(os.walk(inppath), (None, None, []))[2]
 allLines = []
 longest = 0
 for f in files:
-  allLines.append(open(inppath + f).read().split('\n'))
+  allLines.append(open(inppath + f, encoding = 'utf-8').read().split('\n'))
   if (len(allLines[-1]) > longest):
     longest = len(allLines[-1])
 for x in allLines:
@@ -17,7 +17,7 @@ for x in allLines:
     x.append('')
 rotated = list(zip(*allLines))
 
-outputfile = open(outpath + 'output.txt', 'w')
+outputfile = open(outpath + 'output.txt', 'w', encoding = 'utf-8')
 for x in rotated:
   line = ''.join(x)
   if (line != ''):
