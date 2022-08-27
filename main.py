13ae7ef4d24bb117b9e73b3657bd9c78f19244e0
sabyr-cpu <sab.sabyr@gmail.com>
2022-08-27 22:48:12 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+abs = os.path.dirname(__file__)
+inp = 'inputfiles\\'
+out = 'out\\'
+inppath = os.path.join(abs, inp)
+outpath = os.path.join(abs, out)
+files = next(os.walk(inppath), (None, None, []))[2]
+
+allLines = []
+longest = 0
+for f in files:
+  allLines.append(open(inppath + f).read().split('\n'))
+  if (len(allLines[-1]) > longest):
+    longest = len(allLines[-1])
+for x in allLines:
+  for i in range(longest):
+    x.append('')
+rotated = list(zip(*allLines))
+
+outputfile = open(outpath + 'output.txt', 'w')
+for x in rotated:
+  line = ''.join(x)
+  if (line != ''):
+    outputfile.write(line + '\n')
+
+
